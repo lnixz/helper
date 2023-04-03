f33948d2a55a9a11ab2114888b51091f6acd5281
chenjie <873444654@qq.com>
2023-04-03 12:57:34 +0800

    新增引入路劲

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import deleteAttributeInObject from './deleteAttributeInObject/index.js';
 import extractType from './extractType/index.ts';
 import getSearchPathOfTargetObject from './getSearchPathOfTargetObject/index.js';
 import omit from './omit/index.js';
+import copyObj from "./copyObject/index";
 
 export const helper = {
     omit,
     deleteAttributeInObject,
     extractType,
     getSearchPathOfTargetObject,
+    copyObj
 }
